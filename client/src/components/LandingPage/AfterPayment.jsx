d1cded852e16fc4538aeea42f1e37f426cd199bb
Luis Cardona <cardona-luis@outlook.com>
2023-05-20 20:59:04 -0500

    FixedBugs

--- client/src/components/LandingPage/AfterPayment.jsx
+++ client/src/components/LandingPage/AfterPayment.jsx
@@ -7,7 +7,7 @@ function AfterPayment(props) {
     <div>
         <HeroBanner></HeroBanner>
         <main className='Form-Container'>
-        <iframe title='FormPayment' width="640px" height="1080px" src="https://forms.office.com/Pages/ResponsePage.aspx?id=DQSIkWdsW0yxEjajBLZtrQAAAAAAAAAAAAN__lqWrRdUMFU1RE1CMDlQS0tPRk5XN1EzRzZXNElOMy4u&embed=true" frameborder="0" marginwidth="0" marginheight="0" style={{border: "none", maxWidth: "100%", maxHeight:"120vh"}} allowfullscreen webkitallowfullscreen mozallowfullscreen msallowfullscreen> </iframe>
+        <iframe title='FormPayment' width="2640px" height="1080px" src="https://forms.office.com/Pages/ResponsePage.aspx?id=DQSIkWdsW0yxEjajBLZtrQAAAAAAAAAAAAN__lqWrRdUMFU1RE1CMDlQS0tPRk5XN1EzRzZXNElOMy4u&embed=true" frameborder="0" marginwidth="0" marginheight="0" style={{border: "none", maxWidth: "100%", maxHeight:"120vh"}} allowfullscreen webkitallowfullscreen mozallowfullscreen msallowfullscreen> </iframe>
         </main>
  
         <ToolsTechnologies></ToolsTechnologies>
