6bb2989a2ba1d7aad24cb358f16cd11eb7f2da4f
Luis Cardona <cardona-luis@outlook.com>
2023-05-22 17:33:50 -0500

    Portolio

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Footer(props) {
+  return (
+    <div className='footer-container'>
+        <a href="/"> LC Website Solutions</a>
+        
+    </div>
+  )
+}
+
+
+export default Footer
