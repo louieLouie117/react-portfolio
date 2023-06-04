bf592b4d48c1751913562a47bfd28b50e8cf08cb
Luis Cardona <cardona-luis@outlook.com>
2023-06-03 20:33:27 -0500

    ServcesAdded

--- client/src/components/LandingPage/HeroBanner.jsx
+++ client/src/components/LandingPage/HeroBanner.jsx
@@ -80,10 +80,7 @@ const ContactMeHandler = e =>{
                     <h2>{timeLabel},</h2>
                     <h3>my name is </h3>
                     <h1>Luis Cardona</h1>
-                  <p>LC Website Solutions</p>
-
-
-            
+                    <p>LC Website Solutions</p>
                 
                   </header>
                   
@@ -91,7 +88,7 @@ const ContactMeHandler = e =>{
                   <img id="PortfolioImg" src="/img/PortfolioPhoto350.png" alt="" />
                   </div>
                   <footer>
-                  <h2>I help businesses in Montgomery TN get online.</h2>
+                  <h2>I help <br className='BrakePoint'></br>businesses <br className='BrakePoint'></br>get online, <br className='BrakePoint'></br>market online, <br className='BrakePoint'></br>and remain online.</h2>
 
                   </footer>
 
@@ -100,7 +97,7 @@ const ContactMeHandler = e =>{
                       <button>My Services</button>
                     </a> */}
 
-                    <button onClick={ContactMeHandler}>Contact me</button>
+                    {/* <button onClick={ContactMeHandler}>Contact me</button> */}
 
                     </div>
          
@@ -147,12 +144,12 @@ const ContactMeHandler = e =>{
 
                     </div>
 
-      
+{/*       
         <div className="mySkills-container">
      
             
 
-              {/* <ul>
+              <ul>
           
                 <li><p>Problem-solving</p></li>
                 <li><p>Critical Thinking</p></li>
@@ -180,9 +177,9 @@ const ContactMeHandler = e =>{
           
 
 
-          </ul> */}
+          </ul>
          
-      </div>
+      </div> */}
      
         
     </div>
--- client/src/components/LandingPage/WorkExperience.jsx
+++ client/src/components/LandingPage/WorkExperience.jsx
@@ -30,6 +30,55 @@ function WorkExperience(props) {
           
                   <ul>   
 
+                  <li>
+                      
+                      <img src="img/FreeWebsites/FreeWebsite1.png" alt=''/>
+                 
+               
+
+                              <footer className="CardFooter-Container">   
+                                  <a href="https://buy.stripe.com/aEU6rFcPy7qj6xGeUV">                                                                       
+                                  <button  id='FreeWebsite'>Purchase Website</button> 
+                                  </a>
+
+                                  <a href="https://icy-flower-0ac939810.1.azurestaticapps.net">View</a>
+
+                              </footer>
+                      </li>
+                
+
+                    <li>
+
+                   
+                      <img src="img/FreeWebsites/FreeWebsite2.png" alt=''/>
+                     
+
+                                <footer className="CardFooter-Container">   
+                                <a href="https://buy.stripe.com/aEU6rFcPy7qj6xGeUV">                                                                       
+                                  <button   id='FreeDesign'>Purchase Website</button> 
+                                  </a>
+                                  <a href="https://xd.adobe.com/view/a0dfff20-de8d-4e46-6b67-bbfc029b973f-d6ee/?fullscreen">View</a>
+
+                              </footer>
+                      </li>
+
+                      
+                    <li>
+
+                     
+                      <img src="img/FreeWebsites/FreeWebsite3.png" alt=''/>
+                      
+
+
+                      <footer className="CardFooter-Container">   
+                                <a href="https://buy.stripe.com/aEU6rFcPy7qj6xGeUV">                                                                       
+                                  <button  id="FreeDesign">Purchase Website</button> 
+                                  </a>
+                                  <a href="https://xd.adobe.com/view/2c35bed9-b4dd-409f-9606-5815b7d37875-7966/?fullscreen">View</a>
+
+                              </footer>
+                      </li>
+
                       <li>
                          
                             <img src="img/Portfolio/GarageSallyWebsite.png" alt=''/>                          
@@ -169,54 +218,7 @@ function WorkExperience(props) {
                        
 
 
-                    <li>
-                      
-                      <img src="img/FreeWebsites/FreeWebsite1.png" alt=''/>
-                 
-               
-
-                              <footer className="CardFooter-Container">   
-                                  <a href="https://buy.stripe.com/aEU6rFcPy7qj6xGeUV">                                                                       
-                                  <button  id='FreeWebsite'>Get Free Website</button> 
-                                  </a>
-
-                                  <a href="https://icy-flower-0ac939810.1.azurestaticapps.net">View</a>
-
-                              </footer>
-                      </li>
-                
-
-                    <li>
-
-                   
-                      <img src="img/FreeWebsites/FreeWebsite2.png" alt=''/>
-                     
-
-                                <footer className="CardFooter-Container">   
-                                <a href="https://buy.stripe.com/aEU6rFcPy7qj6xGeUV">                                                                       
-                                  <button   id='FreeDesign'>Get Free Website</button> 
-                                  </a>
-                                  <a href="https://xd.adobe.com/view/a0dfff20-de8d-4e46-6b67-bbfc029b973f-d6ee/?fullscreen">View</a>
-
-                              </footer>
-                      </li>
-
-                      
-                    <li>
-
-                     
-                      <img src="img/FreeWebsites/FreeWebsite3.png" alt=''/>
-                      
-
-
-                      <footer className="CardFooter-Container">   
-                                <a href="https://buy.stripe.com/aEU6rFcPy7qj6xGeUV">                                                                       
-                                  <button  id="FreeDesign">Get Free Website</button> 
-                                  </a>
-                                  <a href="https://xd.adobe.com/view/2c35bed9-b4dd-409f-9606-5815b7d37875-7966/?fullscreen">View</a>
-
-                              </footer>
-                      </li>
+                  
 
                  
                     <li></li>
@@ -231,28 +233,7 @@ function WorkExperience(props) {
                   </div>
 
                   
-                  <div className='CustomWebsite-Container' >
-
-                  <section className='AlignTop'>
-                    <h1>Having a website is crucial for any business in today’s digital age. </h1>
-                    <p>I can design, develop, and maintain your website.</p>
-                    <a  href="https://buy.stripe.com/aEU6rFcPy7qj6xGeUV">
-                    <button className='MainBTN' >View My Services</button>
-                    </a>
-                  </section>
-
-                  <picture>
-                     <source media="(min-width: 1100px)" srcset="/img/DesktopFocusMobile.png"/>               
-                    <img src="/img/MobileWebsite.png" alt="" />
-                  </picture>
-                    
-                      <section>
-                      <p>A website makes your business appear more professional and credible, and it allows you to reach a wider audience than ever before.</p>
-                      <p>With a well-designed website, you can generate leads and sales for your business, showcase your work, and provide valuable information to repeat customers. </p>
-                      <p>In addition, through the use of modern SEO tactics, you can beat your competitors online and establish yourself as a leader in your industry.</p>
-                      </section>
-             
-                  </div>
+               
 
                   </main>
 
