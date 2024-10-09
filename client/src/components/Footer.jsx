63706010ee7db1ed6354173b4dcd343913bfba72
Luis Cardona <cardona-luis@outlook.com>
2024-10-09 11:17:22 -0500

    NewLayoutPricing

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer(props) {
   return (
     <div className='footer-container'>
-        <a href="/">LC Website Solutions</a>
+        <a href="/">LC Digital Solutions</a>
         
     </div>
   )
